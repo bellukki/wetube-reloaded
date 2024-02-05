74db1951e4434cb0af6baf21b98339c0ad04403d
belluki <belluk.kim@gmail.com>
2024-02-05 17:48:41 +0900

    #5.7 Conditionals

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videowRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("I will respond.");
-  return res.send("<h1>I still love you.</h1>");
-};
-const login = (req, res) => {
-  console.log("I will respond.");
-  return res.send("<h1>I still love you so much.</h1>");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handelListening = () =>
   console.log(`✅ Server lsitening on port http://localhost:${PORT} 🌠`);
