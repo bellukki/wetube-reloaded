91f08beae3eb498886110c76bd8a356253a0dc58
belluki <belluk.kim@gmail.com>
2024-03-13 22:40:44 +0900

    #16

--- src/models/Video.js
+++ src/models/Video.js
@@ -9,11 +9,11 @@ const videoSchema = new mongoose.Schema({
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
-    comments: [
-      { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
-    ],
-    rating: { type: Number, default: 0, required: true },
   },
+  comments: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
+  rating: { type: Number, default: 0, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
